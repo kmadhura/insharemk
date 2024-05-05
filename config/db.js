a937457148b54307a080d3afd01f8593183ce468
kmadhura <kmadhura777@gmail.com>
2024-05-05 17:17:23 +0530

    initial

---
+++ config/db.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const mongoose=require ('mongoose')
+
+function connectDB(){
+    //Database connection
+
+ mongoose.connect(process.env.MONGO_CONNECTION_URL,{useNewUrlParser:true})
+
+ const connection=mongoose.connection;
+
+ connection.once('open',()=>{
+    console.log('Database connected........')
+ }).on('error',function(err){
+    console.log(err)
+ })
+
+}
+
+module.exports=connectDB;
